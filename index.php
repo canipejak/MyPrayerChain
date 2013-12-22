a52854f82d455a1eb981d01f34a4130fb8a9b039
pleek91 <pleek91@gmail.com>
2013-12-22 12:15:42 -0600

    Created index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>My Prayer Chain</title>
+
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<?php
+echo "Hello World!</br>";
+?>
+This is a php page :)
+</body>
+</html>
